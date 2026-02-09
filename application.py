78a75d0ca9d3e055ebdacbb570bcaa92c4e71eb2
rvazqu17 <rvazqu17@students.kennesaw.edu>
2026-02-08 21:33:31 -0500

    change text

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><body><h1>Hello!</h1></body></html>\n"
+    return "<html><body><h1>Hello!</h1></body></html>\n <html><body>My ksunetid is rvazqu17<body><html>\n"
